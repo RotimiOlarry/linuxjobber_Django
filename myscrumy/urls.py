6307e158658e6fb626c9695454e8028e30c6e7e2
Rotimi Olarry <timiolarry@gmail.com>
2020-05-14 00:06:49 +0100

    getting there

--- myscrumy/urls.py
+++ myscrumy/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls import url
 
 
 urlpatterns = [
-    path('timiolarryscrumy/', include('timiolarryscrumy.urls')),
+    path('', include('timiolarryscrumy.urls')),
     path('admin/', admin.site.urls),
     #url(r'^timiolarryscrumy/',),
 ]
