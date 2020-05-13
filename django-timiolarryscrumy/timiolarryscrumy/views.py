719c349bce821ea830e00e42fb2a85fe9c4909ae
Rotimi Olarry <timiolarry@gmail.com>
2020-05-13 23:15:19 +0100

    tweaked

--- django-timiolarryscrumy/timiolarryscrumy/views.py
+++ django-timiolarryscrumy/timiolarryscrumy/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 # Create your views here.
 def get_grading_parameters(request):
     return HttpResponse("Welcome to Django")
+    return HttpResponse("This is a Scrum Application")
 
 
     
