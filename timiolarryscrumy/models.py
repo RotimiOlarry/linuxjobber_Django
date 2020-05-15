291f0555e473e22223f8d3b3ea3b2c8ac31de3fa
Rotimi Olarry <timiolarry@gmail.com>
2020-05-15 17:58:03 +0100

    more than 60%

--- timiolarryscrumy/models.py
+++ timiolarryscrumy/models.py
@@ -11,6 +11,7 @@ class GoalStatus(models.Model):
 
 
 class ScrumyGoal(models.Model):
+    goal_name =models.CharField(max_length=50, default='')
     user = models.ForeignKey(User, related_name = 'goal_created',on_delete=models.PROTECT)
     goal_name =models.CharField(max_length=50, default='')
     goal_id = models.IntegerField(default=10)
@@ -31,7 +32,7 @@ class ScrumyHistory(models.Model):
     time_of_action = models.TimeField(auto_now=False, auto_now_add=False)
 
     def __str__(self):
-        return self.goal
+        return self.created_by
 
 
     
