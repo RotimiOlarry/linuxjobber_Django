69262f674f053063e1b2b1820c3c771133dbb62e
Rotimi Olarry <timiolarry@gmail.com>
2020-05-17 01:56:28 +0100

    Saturday lab 10

--- timiolarryscrumy/urls.py
+++ timiolarryscrumy/urls.py
@@ -1,7 +1,12 @@
-from django.conf.urls import url 
+from django.conf.urls import url
+#from django.urls import path
 from timiolarryscrumy import views
 
 
 urlpatterns = [ 
-    url(r'^$', views.index, name = 'index'),
+    url(r'', views.index, name = 'index'),
+    #url(r'^/move_goal/(?P<poll_id>\d+)/$', views.move_goal, name = "move_goal"),
+    #r'^(?P<pk>\d+)/$'
+    url(r'^/move_goal/(?P<pk>\d+)/$', views.move_goal, name = "move_goal"),
+    #r'^move_goal/(\d+)/$'
 ]
--- timiolarryscrumy/views.py
+++ timiolarryscrumy/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from timiolarryscrumy.models import *
 
 
 # Create your views here.
 def index(request):
     #return HttpResponse("Welcome to Django")
-    return HttpResponse("This is a Scrum Application")
+    response = ScrumyGoals.objects.filter(goal_name ="Learn Django")
+    return HttpResponse(response)
+
+def move_goal(request, goal_id):
+    resp = ScrumyGoals.objects.get(pk = 1)
+    return HttpResponse(resp)
 
 
     
